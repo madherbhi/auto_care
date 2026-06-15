ee295976c824563dbef23d27a19b89e620446ce2
madherbhiT <madherbhi.t@branchx.in>
2026-06-15 23:03:15 +0530

    chnaged package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.auto_care"
+    namespace = "com.gnvi.autocare"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,11 +21,11 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.auto_care"
+        applicationId = "com.gnvi.autocare"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
--- android/app/src/main/kotlin/com/example/auto_care/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/auto_care/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.auto_care
+package com.gnvi.autocare
 
 import io.flutter.embedding.android.FlutterActivity
 
